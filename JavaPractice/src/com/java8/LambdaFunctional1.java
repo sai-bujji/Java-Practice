2e7e129c1dd0796159effbf43dc67a2d951c9f1c
saive <saive@192.168.0.116>
2021-11-16 07:26:11 +0530

    Test PUSH

--- JavaPractice/src/com/java8/LambdaFunctional1.java
+++ JavaPractice/src/com/java8/LambdaFunctional1.java
@@ -1,10 +1,22 @@
 package com.java8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LambdaFunctional1 {
+	
+	public static void testDisplay() {
+		List<String> li = new ArrayList<>();
+		li.add("Hishika");
+		li.add("Sai");
+		li.add("Madhuri");
+		
+		li.forEach((n)->System.out.println(n));
+	}
+	
+		public static void main(String[] args) {
 
-	public static void main(String[] args) {
-
-		System.out.println("Java 8Git push test");
+		testDisplay();
 	}
 
 }
