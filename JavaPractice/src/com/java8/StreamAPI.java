1ab65345261e7c3e9d738c5566a888e5c2df1711
sai-bujji <sai.kuc@gmail.com>
2022-05-16 07:31:55 +0530

    Practice on Streams

---
+++ JavaPractice/src/com/java8/StreamAPI.java
@@ -0,0 +1,73 @@
+package com.java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Product{  
+    int id;  
+    String name;  
+    float price;  
+    public Product(int id, String name, float price) {  
+        this.id = id;  
+        this.name = name;  
+        this.price = price;  
+    }  
+}  
+
+public class StreamAPI {
+
+	public static void main(String[] args) {
+
+		List<Product> productsList = new ArrayList<Product>();  
+        //Adding Products  
+        productsList.add(new Product(1,"HP Laptop",25000f));  
+        productsList.add(new Product(2,"Dell Laptop",30000f));  
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));  
+        productsList.add(new Product(4,"Sony Laptop",28000f));  
+        productsList.add(new Product(5,"Apple Laptop",90000f));
+        productsList.add(new Product(6,"ASUS Laptop",80000f)); 
+        
+        //Ex 1
+        List<Float> pricegreater = productsList.stream().filter(p->p.price>30000).map(p->p.price).collect(Collectors.toList());
+        System.out.println(pricegreater);
+        
+        //Ex 2
+        productsList.stream().filter(p->p.price==30000).forEach(p->System.out.println(p.name));
+        
+        //Ex 3
+        Stream.iterate(1, element->element+1).filter(element->element%5==0)  
+        .limit(5)  
+        .forEach(System.out::println); 
+        
+      //new Sorting
+      		//Collections.sort(li, (p1,p2)->{return p1.compareTo(p2);});
+      				
+      		/*
+      		 * //Old sorting Collections.sort(li, new Comparator<String>() {
+      		 * 
+      		 * @Override public int compare(String o1, String o2) { return o1.compareTo(o2);
+      		 * } });
+      		 */
+      		
+      		/*
+      		 * Collections.sort(persons, Comparator.comparing(Person::getFname)
+      		 * .thenComparingInt(Person::getAge));
+      		 */
+      		
+      		
+      		
+
+      		/*
+      		 * Map<String, Map<String, List<Employee>>> multipleFieldsMapList =
+      		 * employeesList.stream() .collect(
+      		 * Collectors.groupingBy(Employee::getDesignation,
+      		 * Collectors.groupingBy(Employee::getGender)));
+      		 */
+      		
+        
+	}
+
+}
