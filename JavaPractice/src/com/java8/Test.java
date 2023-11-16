d2ec433372d59ba3c1d2012388686c8b41440f46
Hishika <Hishika@DESKTOP-4QMHS6E>
2023-11-16 18:26:23 +0530

    Initial commit

--- JavaPractice/src/com/java8/Test.java
+++ JavaPractice/src/com/java8/Test.java
@@ -1,10 +1,45 @@
 package com.java8;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String str="aabbccddaabbcbbaa";
+		
+		System.out.println("Longist palindrome in a given String:   "+ longestPalindrome(str));
 	}
 
+	private static String longestPalindrome(String str) {
+		if(null == str) {
+			return "";
+		}
+		String longestString =String.valueOf(str.charAt(0));
+		
+		for(int i=0; i<= str.length()-1;i++) {
+			
+			String st = longestString(str, i, i);
+			if(longestString.length()< st.length()) {
+				longestString = st;
+			}
+		}
+		
+		return longestString;
+	}
+
+	private static String longestString(String str, int left, int right) {
+
+		while(left>=0 && right<str.length() && str.charAt(left)== str.charAt(right)) {
+			left--;
+			right++;
+		}
+		
+		return str.substring(left+1, right);
+	}
+	
 }
